fc9a89d8b8e03bcbe2f393af12d16679eba9ba4d
zWaR <zwar_the_one@hotmail.com>
2018-12-30 21:33:40 -0500

    Improved comment accuracy

--- src/singleton/try-singleton.ts
+++ src/singleton/try-singleton.ts
@@ -13,7 +13,7 @@ class TrySingleton {
     console.log(`instance1: ${instance1.variable}`);
 
     // const instance3 = new Singleton(); -> Instantiating a singleton results in an error, since the constructor is private.
-    // (With my tsconfig configuration it won't even compile)
+    // (With my tsconfig configuration, it won't even transpile)
 
   }
 
